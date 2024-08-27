ae9cac119873d9666f5f346e6f66b3119511a1c5
unknown <adamski-piotrek@wp.pl>
2024-08-27 21:49:51 +0200

    Init

---
+++ src/main/kotlin/com/pada/medmaster/MedmasterApplication.kt
@@ -0,0 +1,11 @@
+package com.pada.medmaster
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MedmasterApplication
+
+fun main(args: Array<String>) {
+	runApplication<MedmasterApplication>(*args)
+}
---
+++ src/test/kotlin/com/pada/medmaster/MedmasterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pada.medmaster
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MedmasterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
