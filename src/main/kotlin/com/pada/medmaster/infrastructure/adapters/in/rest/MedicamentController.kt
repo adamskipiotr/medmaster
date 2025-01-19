f7110d5296df817264f423da60fc7b1f969a77a3
unknown <adamski-piotrek@wp.pl>
2025-01-19 14:42:37 +0100

    Describe Controllers in OpenAPI

--- src/main/kotlin/com/pada/medmaster/infrastructure/adapters/in/rest/MedicamentController.kt
+++ src/main/kotlin/com/pada/medmaster/infrastructure/adapters/in/rest/MedicamentController.kt
@@ -2,17 +2,38 @@ package com.pada.medmaster.infrastructure.adapters.`in`.rest
 
 import com.pada.medmaster.application.dto.request.medicament.MedicamentRequestDTO
 import com.pada.medmaster.application.ports.`in`.CreateMedicamentUseCase
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/medicaments")
+@Tag(name = "Medicaments", description = "API for managing Medicaments")
 class MedicamentController(
     private val createMedicamentUseCase: CreateMedicamentUseCase
 ) {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(
+        summary = "Create a new medicament",
+        description = "Adds a new medicament.",
+        requestBody = io.swagger.v3.oas.annotations.parameters.RequestBody(
+            description = "The medicament data to create.",
+            required = true
+        ),
+        responses = [
+            io.swagger.v3.oas.annotations.responses.ApiResponse(
+                responseCode = "201",
+                description = "Medicament successfully created."
+            ),
+            io.swagger.v3.oas.annotations.responses.ApiResponse(
+                responseCode = "400",
+                description = "Invalid request data."
+            )
+        ]
+    )
     fun createTreatment(@RequestBody medicamentRequestDTO: MedicamentRequestDTO) {
         createMedicamentUseCase.create(medicamentRequestDTO)
     }
--- src/main/kotlin/com/pada/medmaster/infrastructure/adapters/in/rest/PatientController.kt
+++ src/main/kotlin/com/pada/medmaster/infrastructure/adapters/in/rest/PatientController.kt
@@ -2,17 +2,39 @@ package com.pada.medmaster.infrastructure.adapters.`in`.rest
 
 import com.pada.medmaster.application.dto.request.patient.PatientRequestDTO
 import com.pada.medmaster.application.ports.`in`.CreatePatientUseCase
+import com.pada.medmaster.domain.model.treatment.Treatment
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/patients")
+@Tag(name = "Patients", description = "API for managing Patients")
 class PatientController(
     private val createPatientUseCase: CreatePatientUseCase
 ) {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(
+        summary = "Create a new patient",
+        description = "Adds a new patient.",
+        requestBody = io.swagger.v3.oas.annotations.parameters.RequestBody(
+            description = "The patient data to create.",
+            required = true
+        ),
+        responses = [
+            io.swagger.v3.oas.annotations.responses.ApiResponse(
+                responseCode = "201",
+                description = "Patient successfully created."
+            ),
+            io.swagger.v3.oas.annotations.responses.ApiResponse(
+                responseCode = "400",
+                description = "Invalid request data."
+            )
+        ]
+    )
     fun createPatient(@RequestBody patientRequestDTO: PatientRequestDTO) {
         createPatientUseCase.create(patientRequestDTO)
     }
